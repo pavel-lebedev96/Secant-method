01ecce5c245b8f67834087a6bf66a22e6f2e9f9b
pavel-lebedev96 <73466790+pavel-lebedev96@users.noreply.github.com>
2021-02-09 19:52:50 +0700

    Add files via upload

--- ConsoleApplication2/1.asm
+++ ConsoleApplication2/1.asm
@@ -10,8 +10,8 @@ PREV_C REAL4 ?
 CONST_05 REAL4 0.5
 
 .CODE
-;вычисление натурального логарифма
-;аргумент и результат в ST(0)
+;РІС‹С‡РёСЃР»РµРЅРёРµ РЅР°С‚СѓСЂР°Р»СЊРЅРѕРіРѕ Р»РѕРіР°СЂРёС„РјР°
+;Р°СЂРіСѓРјРµРЅС‚ Рё СЂРµР·СѓР»СЊС‚Р°С‚ РІ ST(0)
 NAT_LOG PROC
 	FLD1; ST(0)=1, ST(1)=x
 	FXCH ST(1); ST(0)=x, ST(1)=1
@@ -21,8 +21,8 @@ NAT_LOG PROC
 	RET
 NAT_LOG ENDP
 
-;исходная функция
-;аргумент и результат в ST(0)
+;РёСЃС…РѕРґРЅР°СЏ С„СѓРЅРєС†РёСЏ
+;Р°СЂРіСѓРјРµРЅС‚ Рё СЂРµР·СѓР»СЊС‚Р°С‚ РІ ST(0)
 FUNC PROC
 	FLD CONST_05; ST(0)=0.5, ST(1)=x
 	FADD ST,ST(1); ST(0)=0.5+x, ST(1)=x
@@ -33,8 +33,8 @@ FUNC PROC
 	RET
 FUNC ENDP
 
-;нахождение пересечения хорды с осью абсцисс
-;результат в ST(0)
+;РЅР°С…РѕР¶РґРµРЅРёРµ РїРµСЂРµСЃРµС‡РµРЅРёСЏ С…РѕСЂРґС‹ СЃ РѕСЃСЊСЋ Р°Р±СЃС†РёСЃСЃ
+;СЂРµР·СѓР»СЊС‚Р°С‚ РІ ST(0)
 CALC_C PROC
 	FLD REAL4 PTR [EBP+12]; ST(0)=b
 	FLD REAL4 PTR [EBP+8]; ST(0)=a, ST(1)=b
@@ -50,14 +50,14 @@ CALC_C PROC
 	RET
 CALC_C ENDP
 
-;вызываемая подпрограмма
+;РІС‹Р·С‹РІР°РµРјР°СЏ РїРѕРґРїСЂРѕРіСЂР°РјРјР°
 SOLVE PROC
 	PUSH EBP
 	MOV EBP,ESP
 	; a=EBP+8
 	; b=EBP+12
 	; eps=EBP+16
-	FINIT; инициализация сопроцессора
+	FINIT; РёРЅРёС†РёР°Р»РёР·Р°С†РёСЏ СЃРѕРїСЂРѕС†РµСЃСЃРѕСЂР°
 	FLD REAL4 PTR [EBP+8]; ST(0)=a
 	CALL FUNC; ST(0)=f(a)
 	FSTP F_A; f_a=f(a)
@@ -76,7 +76,7 @@ METHOD:
 	FABS; ST(0)=|cur_c-prev_c|, ST(1)=eps
 	FCOMIP ST,ST(1); ST(0)=|cur_c-prev_c|
 	FSTP ST
-	; если abs(cur_c-prev_c) < eps, то переход END_METHOD
+	; РµСЃР»Рё abs(cur_c-prev_c) < eps, С‚Рѕ РїРµСЂРµС…РѕРґ END_METHOD
 	JC END_METHOD;
 	INC N
 	FLD CUR_C; ST(0)=cur_c
@@ -93,7 +93,7 @@ METHOD:
 	FSTP ST
 	FLD F_C; ST(0)=f_c
 	FLD CUR_C; ST(0)=cur_c, ST(1)=f_c
-	;если f(a)*f(c) < 0, то переход LESS
+	;РµСЃР»Рё f(a)*f(c) < 0, С‚Рѕ РїРµСЂРµС…РѕРґ LESS
 	JC LESS
 	FSTP REAL4 PTR [EBP+8]; a=cur_c, ST(0)=f_c
 	FSTP F_A; f_a=f_c
